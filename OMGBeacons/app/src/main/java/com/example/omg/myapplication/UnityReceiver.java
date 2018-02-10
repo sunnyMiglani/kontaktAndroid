09c24321df9cc0d27463ecb6d6e3b3d2fc41baff
sydKnee <sd14814@my.bristol.ac.uk>
2018-02-10 22:26:13 +0000

    Compiles a proper .jar now, fixed the spelling and other code readability issues.

--- OMGBeacons/app/src/main/java/com/example/omg/myapplication/UnityReceiver.java
+++ OMGBeacons/app/src/main/java/com/example/omg/myapplication/UnityReceiver.java
@@ -4,18 +4,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class UnityReciever extends BroadcastReceiver {
+public class UnityReceiver extends BroadcastReceiver {
 
-    private static UnityReciever instance;
+    private static UnityReceiver instance;
     private static final String EXTRA_BEACON_NAME = "com.example.omg.myapplication.extra.BEACON_NAME";
     public static String intentMessage;
 
+    public UnityReceiver(){
+        instance = this;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        String recieveString = intent.getStringExtra(EXTRA_BEACON_NAME);
-        if (recieveString != null) {
+        String receiveString = intent.getStringExtra(EXTRA_BEACON_NAME); /* Retrieve extended data from the intent. */
+        if (receiveString != null) {
             // We assigned it to our static variable
-            intentMessage = recieveString;
+            intentMessage = receiveString;
         }
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
@@ -24,6 +28,6 @@ public class UnityReciever extends BroadcastReceiver {
     public static void createInstance()
     {
         if(instance ==  null)
-            instance = new UnityReciever();
+            instance = new UnityReceiver();
     }
 }
--- OMGBeacons/app/src/main/java/com/example/omg/myapplication/UnityService.java
+++ OMGBeacons/app/src/main/java/com/example/omg/myapplication/UnityService.java
@@ -14,33 +14,14 @@ import android.content.Context;
 public class UnityService extends IntentService {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "com.example.omg.myapplication.action.FOO";
-    private static final String ACTION_BAZ = "com.example.omg.myapplication.action.BAZ";
     private static final String ACTION_SEND_UNITY = "com.example.omg.myapplication.action.SEND_UNITY";
-
     // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "com.example.omg.myapplication.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "com.example.omg.myapplication.extra.PARAM2";
     private static final String EXTRA_BEACON_NAME = "com.example.omg.myapplication.extra.BEACON_NAME";
 
     public UnityService() {
         super("UnityService");
     }
 
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, UnityService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
 
     /**
      * Starts this service to perform action Baz with the given parameters. If
@@ -48,19 +29,10 @@ public class UnityService extends IntentService {
      *
      * @see IntentService
      */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, UnityService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
     public static void startActionSendUnity(Context context, String param1) {
         Intent intent = new Intent(context, UnityService.class);
         intent.setAction(ACTION_SEND_UNITY);
-        intent.putExtra(EXTRA_BEACON_NAME, param1);
+        intent.putExtra(EXTRA_BEACON_NAME, param1); /*Add extended data to the intent. The name must include a package prefix*/
         context.startService(intent);
     }
 
@@ -78,23 +50,9 @@ public class UnityService extends IntentService {
     }
 
     /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    /**
      * Handle action Baz in the provided background thread with the provided
      * parameters.
      */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
     private void handleActionSendToUnity(Intent intent) {
         sendBroadcast(intent);
     }
